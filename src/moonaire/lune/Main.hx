0c6293c850ff02a4dd7cc41eaac59d406ca0361e
Munir Hussin <profoundseven+github@gmail.com>
2012-01-05 18:24:52 +0800

    added files

---
+++ src/moonaire/lune/Main.hx
@@ -0,0 +1,16 @@
+package moonaire.lune;
+
+import nme.Lib;
+
+/**
+ * ...
+ * @author Munir Hussin
+ */
+
+class Main 
+{
+	static public function main() 
+	{
+        var lune:Lune = new Lune();
+	}
+}
